2287ee7fefbfaa0658b64459913f08c4649786c2
Marshall Mattingly <marshall.mattingly@gmail.com>
2015-02-18 17:30:36 -0600

    More map stuff.

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,13 @@
 shp("/maps/nd_counties.zip").then(function(data) {
-    var map = L.map('map');
+    var map = L.map('map').setView([47, 100], 13);
+    //L.geoJson(data).addTo(map);
+    console.log('shit');
     L.geoJson(data).addTo(map);
+    /*
+    data.features.forEach(function(geo) {
+        L.geoJson(geo).addTo(map);
+        console.log(geo);
+    });
+    */
 });
+
