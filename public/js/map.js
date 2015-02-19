09b872905dd3b330c04e2412fd94cfcff47a7fd1
Marshall Mattingly <marshall.mattingly@gmail.com>
2015-02-19 15:43:46 -0600

    Made the map better.
    Added threading to the map so it loads in the background.
    Added spinner to the map element to show loading status.

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,36 @@ $('#map,#narrative').css({
     'height': $(window).height()-50
 });
 
-shp("/maps/nd_counties.zip").then(function(data) {
+// insert spinner for now
+var opts = {
+    lines: 13, // The number of lines to draw
+    length: 20, // The length of each line
+    width: 10, // The line thickness
+    radius: 30, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '25%' // Left position relative to parent
+};
+var target = document.getElementById('map');
+var spinner = new Spinner(opts).spin(target);
+
+var worker = cw(function(base,cb){
+    importScripts('/js/shp.min.js');
+    shp(base).then(cb);
+});
+
+//worker can be called multiple times
+worker.data(cw.makeUrl('/maps/nd_counties.zip')).then(function(data){
+    spinner.stop();
     var map = L.map('map').setView([47, -97.5], 7.5);
     var myStyle = {
         "color": "#ff0000",
@@ -22,9 +51,6 @@ shp("/maps/nd_counties.zip").then(function(data) {
     }).addTo(map);
     console.log(data.features);
 });
-
-
-
 
 $('#slider').click(function() {
     if ($(this).hasClass('expanded')) {
