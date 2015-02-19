2f210e173a98eda8b935f3fafb20a9ed3ef0458f
Marshall Mattingly <marshall.mattingly@gmail.com>
2015-02-18 21:08:33 -0600

    Added county name popup.

--- public/js/map.js
+++ public/js/map.js
@@ -5,9 +5,16 @@ shp("/maps/nd_counties.zip").then(function(data) {
         "weight": 5,
         "opacity": 0.65
     };
-    
+  
+    function onEachFeature(feature, layer) {
+        if (feature.properties && feature.properties.NAME) {
+            layer.bindPopup(feature.properties.NAME);
+        }
+    }
+
     L.geoJson(data.features, {
-        style: myStyle
+        style: myStyle,
+        onEachFeature: onEachFeature
     }).addTo(map);
     console.log(data.features);
 });
