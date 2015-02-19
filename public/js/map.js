60af9edb262bdfd05ded58e7109d07045f8e8467
Marshall Mattingly <marshall.mattingly@gmail.com>
2015-02-18 21:01:07 -0600

    Now actually showing ND.
    TODO: Background, popups.

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,14 @@
 shp("/maps/nd_counties.zip").then(function(data) {
-    var map = L.map('map').setView([47, 100], 13);
-    //L.geoJson(data).addTo(map);
-    console.log('shit');
-    L.geoJson(data).addTo(map);
-    /*
-    data.features.forEach(function(geo) {
-        L.geoJson(geo).addTo(map);
-        console.log(geo);
-    });
-    */
+    var map = L.map('map').setView([47, -100], 6.5);
+    var myStyle = {
+        "color": "#ff0000",
+        "weight": 5,
+        "opacity": 0.65
+    };
+    
+    L.geoJson(data.features, {
+        style: myStyle
+    }).addTo(map);
+    console.log(data.features);
 });
 
