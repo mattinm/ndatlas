6f4403859cccebadef08858813895d4ca0fdc015
Michael Marti <michael.marti@my.und.edu>
2015-02-24 21:33:41 -0600

    finished initial landing/home page

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var parser = require('body-parser');
 var shp = require('shpjs');
-var homeConfig = require(__dirname + '/utils/HomeConfig');
-var chapters = require(__dirname + '/utils/Chapters');
 var app = express();
-app.set('view engine', 'jade'); // declare we're going to use jade
-app.set('views', __dirname + '/views'); // tell express which directory our jade templates (views) will be in
-app.use(express.static(__dirname + '/public')); // tell express which directory will have our statis (public) assets like js and css
+var stories = {
+    'titles': [
+        'Story One',
+        'Story Two',
+        'Story Three',
+        'Story Four',
+        'Story Five'
+    ]
+};
 
-// these are the routes
+app.set('view engine', 'jade');
+app.set('views', __dirname+'/views');
+app.use(express.static(__dirname+'/public'));
+
 app.get('/', function (req, res) {
-    res.render('home', {    // use res.render to tell express what template to use, in this case it will render home.jade and pass the following object for the template to use
-        'title': 'ND@125',
-        'data': homeConfig
+    res.render('home', {
+        'stories': stories
     });
 });
 
-app.get('/map', function (req, res) {
-    res.render('map', {    // use res.render to tell express what template to use, in this case it will render home.jade and pass the following object for the template to use
-        'title': 'Map',
-        'data': chapters,
-        'edit': req.query.edit
-    });
-});
-
-app.get('/chapters/:chapter', function (req, res) {
-    var chapter = req.params.chapter; // pull value from URL using the :chapter placeholder
-    
-    res.render('chapter', {    // use res.render to tell express what template to use, in this case it will render chapter.jade and pass the following object for the template to use
-        'title': chapters[chapter].title+' &ndash; Chapter '+chapter.substring(0,1).toUpperCase()+chapter.slice(1),
-        'data': chapters,
-        'chapter': chapter
-    });
-});
-
-app.get('/explore', function(req, res) {
-    res.send('you triggered the explore route');    // use res.send to send a string, or res.json to send a js object
-});
-
-app.listen(8005, '127.0.0.1');  // tell express which port and ip to listen on
+app.listen(8005, '127.0.0.1');
