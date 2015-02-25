d08253d2dc4473c291f40271446214ff822586e5
Michael Marti <michael.marti@my.und.edu>
2015-02-25 12:47:25 -0600

    added models and more autogenerated content

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var express = require('express');
 var parser = require('body-parser');
 var shp = require('shpjs');
 var app = express();
-var stories = {
-    'titles': [
-        'Story One',
-        'Story Two',
-        'Story Three',
-        'Story Four',
-        'Story Five'
-    ]
-};
+var stories = require(__dirname+'/utils/StoriesData');
+var slug_to_story_map = {};
+
+for (var i = 0, n = stories.length; i < n; i++) {
+    stories[i].slug = stories[i].title.toLowerCase().replace(/[^a-z ]+/g, '').replace(/ +/g, '-');
+    slug_to_story_map[stories[i].slug] = i;
+}
 
 app.set('view engine', 'jade');
 app.set('views', __dirname+'/views');
@@ -19,6 +17,17 @@ app.use(express.static(__dirname+'/public'));
 app.get('/', function (req, res) {
     res.render('home', {
         'stories': stories
+    });
+});
+
+app.get('/stories/:title', function (req, res) {
+    var i = slug_to_story_map[req.params.title];
+    var story = stories[i];
+    
+    res.render(story.type, {
+        'story': story,
+        'stories': stories,
+        'index': i
     });
 });
 
---
+++ utils/StoriesData.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        'title': 'The Cat in the Hat',
+        'type': 'text',
+        'content': 'The sun did not shine. It was too wet to play. So we sat in the house all that cold, cold, wet day. I sat there with sally. We sat there, we two. And I said, \'how I wish we had something to do!\' Too wet to go out and too cold to play ball. So we sat in the house. We did nothing at all.'
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'type': 'map',
+        'content': 'I do not like them in a house. I do not like them with a mouse. I do not like them here or there. I do not like them anywhere. I do not like green eggs and ham. I do not like them, Sam-I-Am.'
+    },
+    {
+        'title': 'Oh, the Places You\'ll Go',
+        'type': 'text',
+        'content': 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You\'re on your own. And you know what you know. And YOU are the one who\'ll decide where to go...'
+    }
+];
