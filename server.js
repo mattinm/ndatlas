8a6c6affde203b38f23841e282576c577513658d
Michael Marti <michael.marti@my.und.edu>
2015-02-25 13:33:57 -0600

    optimized server

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 var express = require('express');
 var parser = require('body-parser');
 var shp = require('shpjs');
+var nosql = require('nosql').load(__dirname+'/database.nosql');
 var app = express();
 var stories = [];
 var slug_to_story_map = {};
 
-var nosql = require('nosql').load(__dirname+'/database.nosql');
 nosql.on('load', function() {
-    var map = function(doc) {
-        return doc;
-    };
-
-    var callback = function(selected) {
+    nosql.all(function(a) {
+        return a;
+    }, function(selected) {
         var i = 0;
         selected.forEach(function(o) {
             o.slug = o.title.toLowerCase().replace(/[^a-z ]+/g, '').replace(/ +/g, '-');
             slug_to_story_map[o.slug] = i++;
             stories.push(o);
         });
-    };
-
-    nosql.all(map, callback); 
+    }); 
 });
 
-//      // call after load of DB
-//      nosql.insert(object, [fnCallback]);
-//      nosql.insert(array, fnCallback);
-//
-//      nosql.all(fnMap, fnCallback)
-//      nosql.one(fnMap, fnCallback)
-//      nosql.top(max, fnMap, fnCallback)
-//      nosql.each(fnCallback)
+// call after load of DB
+/*
+nosql.insert(object, [fnCallback]);
+nosql.insert(array, fnCallback);
+nosql.all(fnMap, fnCallback)
+nosql.one(fnMap, fnCallback)
+nosql.top(max, fnMap, fnCallback)
+nosql.each(fnCallback)
+*/
 
 app.set('view engine', 'jade');
 app.set('views', __dirname+'/views');
