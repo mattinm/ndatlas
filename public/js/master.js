5b83c5d950c0ca50f5bf9bf41b8a9e95756bb856
Marshall Mattingly <marshall.mattingly@gmail.com>
2015-04-29 14:05:49 -0500

    Local anchors now scroll fancy

--- public/js/master.js
+++ public/js/master.js
@@ -1,3 +1,15 @@
 $('.disabled').click(function() {
     return false;
 });
+
+// make it so all local anchors scroll
+$(document).ready(function() {
+	$("a[href^=#]").click(function(e) {
+		e.preventDefault();
+		var dest = $(this).attr('href');
+		console.log(dest);
+		$('html,body').animate({
+			scrollTop: $(dest).offset().top
+		}, 'slow');
+	});
+});
