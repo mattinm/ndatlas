8bcec691d7fc4665d0ca00dc3f47fa05a9052557
Lindsey Wingate <lindseymwingate@gmail.com>
2016-10-24 21:45:15 -0500

    adding new pages, formatted students pics, etc..

---
+++ public/js/students.js
@@ -0,0 +1,23 @@
+/*This code is used with the students page for dropdowns*/
+$(document).ready(function() {
+	$('#pic1').on('click', function() {
+		$('#text1').toggle('show');
+	});
+	$('#pic2').on('click', function() {
+		$('#text2').toggle('show');
+	});
+	$('#pic3').on('click', function() {
+		$('#text3').toggle('show');
+	});
+	$('#pic4').on('click', function() {
+		$('#text4').toggle('show');
+	});
+	$('#pic5').on('click', function() {
+		$('#text5').toggle('show');
+	});
+	$('#pic6').on('click', function() {
+		$('#text6').toggle('show');
+	});
+
+
+});
