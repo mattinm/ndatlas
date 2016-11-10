ceb95305d470040bae307524c08c87338c92ac48
Lindsey Wingate <lindseymwingate@gmail.com>
2016-11-10 13:38:20 -0600

    added new js pages

---
+++ public/js/development.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	
+	$(".Fall2014").on("click", function () {
+		$("#Fall2014").toggle();
+	});
+	$(".Spring2015").on("click", function () {
+		$("#Spring2015").toggle("show");
+	});
+
+	$(".Summer2015").on("click", function () {
+		$("#Summer2015").toggle("show");
+	});
+
+	$(".Fall2015").on("click", function () {
+		$("#Fall2015").toggle("show");
+	});
+
+	$(".Spring2016").on("click", function () {
+		$("#Spring2016").toggle("show");
+	});
+});
