b6e580c9406a03996d70d84a2af7c1a3a8d7a0ea
Lindsey Wingate <lindseymwingate@gmail.com>
2016-11-12 17:26:25 -0600

    working on ghost towns now

---
+++ public/js/foreign_born.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$("#a_1890").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1890.png");
+	});
+	$("#a_1900").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1900.png");
+	});
+	$("#a_1910").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1910.png");
+	});
+	$("#a_1920").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1920.png");
+	});
+	$("#a_1930").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1930.png");
+	});
+	$("#a_1940").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1940.png");
+	});
+	$("#a_1950").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1950.png");
+	});
+	$("#a_1970").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1970.png");
+	});
+	$("#a_1980").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1980.png");
+	});
+	$("#a_1990").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1990.png");
+	});
+	$("#a_2000").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_2000.png");
+	});
+	$("#a_2013").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_2013.png");
+	});
+});
---
+++ public/js/ghost_towns.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$("#a_1874").on("click", function() {
+		$("#map").attr("src", "images/maps/GT_C1874.png");
+	});
+	$("#a_1880").on("click", function() {
+		$("#map").attr("src", "images/maps/GT_C1880.png");
+	});
+	$("#a_1885").on("click", function() {
+		$("#map").attr("src", "images/maps/GT_C1885.png");
+	});
+	$("#a_1900").on("click", function() {
+		$("#map").attr("src", "images/maps/GT_C1900.png");
+	});
+	$("#a_1930").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1930.png");
+	});
+	$("#a_1940").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1940.png");
+	});
+	$("#a_1950").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1950.png");
+	});
+	$("#a_1970").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1970.png");
+	});
+	$("#a_1980").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1980.png");
+	});
+	$("#a_1990").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_1990.png");
+	});
+	$("#a_2000").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_2000.png");
+	});
+	$("#a_2013").on("click", function() {
+		$("#map").attr("src", "images/maps/FB_2013.png");
+	});
+});
